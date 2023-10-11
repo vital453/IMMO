0ade79028d51717b57c77a583ae822ab2eb86131
vital453 <mevivital453@gmail.com>
2023-10-12 01:15:13 +0200

    fe

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -187,6 +187,29 @@ export default function Hero() {
             </ListItem>
           </Link>
           <Link
+            to={"/login"}
+            className="no-underline text-black-color"
+            onClick={() => dispatch(setouvre(false))}
+          >
+            <ListItem>
+              <ListItemPrefix>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="h-5 w-5"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M12 2.25a.75.75 0 01.75.75v9a.75.75 0 01-1.5 0V3a.75.75 0 01.75-.75zM6.166 5.106a.75.75 0 010 1.06 8.25 8.25 0 1011.668 0 .75.75 0 111.06-1.06c3.808 3.807 3.808 9.98 0 13.788-3.807 3.808-9.98 3.808-13.788 0-3.808-3.807-3.808-9.98 0-13.788a.75.75 0 011.06 0z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </ListItemPrefix>
+              Se connecter
+            </ListItem>
+          </Link>
+          <Link
             to={"/"}
             className="no-underline text-black-color"
             onClick={() => dispatch(setouvre(false))}
@@ -206,7 +229,7 @@ export default function Hero() {
                   />
                 </svg>
               </ListItemPrefix>
-              Log Out
+              Se déonnecter
             </ListItem>
           </Link>
         </List>
